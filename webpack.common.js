3f7172704b9e82dcae6be08ddbda79496a47c8d9
ld8 <don_lee@me.com>
2020-09-26 21:27:53 +0800

    feat: common config done

---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /.less$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      // 它会应用到普通的 `.js` 文件
+      // 以及 `.vue` 文件中的 `<script>` 块
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      // 它会应用到普通的 `.css` 文件
+      // 以及 `.vue` 文件中的 `<style>` 块
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
+      {
+        test: /\.png$/,
+        use: 'file-loader'
+      }
+    ],
+  },
+  plugins: [
+    // 它的职责是将你定义过的其它规则复制并应用到 .vue 文件里相应语言的块
+    new VueLoaderPlugin(),
+  ],
+};
